67de8c9fa2590848f308940398ee21db6858b289
Laurence Freeman <laurence@laurences-mbp.lan>
2020-09-14 07:04:50 +0100

    Updated process data with new feature to include all data points

--- process_tongue_data.py
+++ process_tongue_data.py
@@ -4,6 +4,8 @@
 import pandas as pd
 import numpy as np
 
+test_file = "/Users/laurence/Desktop/Neuroscience/mproject/data/402.csv"
+
 def process_data(file):
     #Read and create df
     read_data = pd.read_csv(file, header=None)
@@ -23,3 +25,31 @@ def process_data(file):
     df.columns = ["L_Tongue", "R_Tongue","C_Tongue","FO_Tongue","Frames"]
     df = df.drop([1])
     return(df)
+
+def process_data_spout(file):
+    #Read and create df
+    read_data = pd.read_csv(file, header=None)
+    df = pd.DataFrame(read_data)
+    #Change headers
+    new_header = df.iloc[2] #Makes x,y and L column headers
+    df.columns = new_header #Makes x,y and L column headers
+    df = df.drop([0]) #Drops DLC header
+    df = df.drop([2]) #Drops 2nd probability header
+    #Change data types-----------------------------------------------------
+    df.iloc[1:,:] = df.iloc[1:,:].astype(float)
+    #Rename DF Columns-----------------------------------------------------
+    df.columns = ["Frames","LL_GS_X","LL_GS_Y","LL_GS_L",
+                           "LC_GS_X","LC_GS_Y","LC_GS_L",
+                           "LR_GS_X","LR_GS_Y","LR_GS_L",
+                           "RL_CS_X","RL_CS_Y","RL_CS_L",
+                           "RC_CS_X","RC_CS_Y","RC_CS_L",
+                           "RR_CS_X","RR_CS_Y","RR_CS_L",
+                           "LE_T_X", "LE_T_Y", "LE_T_L",
+                           "RE_T_X", "RE_T_Y", "RE_T_L",
+                           "C_T_X", "C_T_Y", "C_T_L",
+                           "F_E_X", "F_E_Y", "F_E_L"]
+    df = df.drop([1])
+    print(df)
+    return(df)
+
+# process_data_spout(test_file)
