396eeccd6107174afa660a4af279e2460611eaeb
Laurence Freeman <laurence@Laurences-MacBook-Pro.local>
2020-09-15 07:06:15 +0100

    Added functionality to detect when licking grape or cherry spout

--- is_licking.py
+++ is_licking.py
@@ -7,7 +7,7 @@ import cv2
 import sys
 
 #Misc----------------------------------------------------------------------
-test_file = "/Users/laurence/Desktop/Neuroscience/mproject/data/Analysed_videos_CSVs_DLC/video_snippet_KM011_2020-03-19_trial90DLC_resnet50_Master_ProjectAug13shuffle1_200000.csv"
+test_file = "/Users/laurence/Desktop/Neuroscience/mproject/data/Analysed_videos_CSVs_DLC/video_snippet_KM011_2020-03-19_trial92DLC_resnet50_Master_ProjectAug13shuffle1_200000.csv"
 #Code to overlay frmes for CML
 #ffmpeg -i video.mov -vf "drawtext=fontfile=Arial.ttf: text=%{n}: x=(w-tw)/2: y=h-(2*lh): fontcolor=white: box=1: boxcolor=0x00000099" -y output.mov
 
@@ -42,15 +42,24 @@ def is_licking_spout(df):
     #Create a dataframe with frames where mouse is licking
     df, frame_is_licking = is_licking(test_file)
 
-    #Is licking left or right spout
+    #Is licking left / grape spout
     is_licking_grape = df.loc[(df['RE_T_X'] <= df["LR_GS_X"])
                              |(df['C_T_X'] <= df["LR_GS_X"])
-                             &(df['LE_T_X'] <= df["LR_GS_X"])]
+                             &(df['LE_T_X'] <= df["LR_GS_X"])
+                             |(df['LE_T_X'] <= df["LC_GS_X"])]
+
+    is_licking_cherry = df.loc[(df['LE_T_X'] >= df["RL_CS_X"])
+                             |(df['C_T_X'] >= df["RL_CS_X"])
+                             &(df['RE_T_X'] >= df["RL_CS_X"])
+                             |(df['RE_T_X'] >= df["RC_CS_X"])]
+
+    frames_licking_cherry = is_licking_cherry.iloc[:,0].values
     frames_licking_grape = is_licking_grape.iloc[:,0].values
-    print("The mouse is licking the Grape spout on these frames:", frames_licking_grape)
+    print("The mouse is licking the Grape spout on these frames:", len(frames_licking_grape))
+    print("The mouse is licking the Cherry spout on these frames:", len(frames_licking_cherry))
 
 #Test
 df, frame_is_licking = is_licking(test_file)
 is_licking_spout(df)
-print("The mouse is licking on these frames:", frame_is_licking)
+print("The mouse is licking on these frames:", len(frame_is_licking))
 # is_licking(sys.argv[1])
