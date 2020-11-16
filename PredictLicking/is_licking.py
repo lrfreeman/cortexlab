52c39b36e85b0f045a366887b1b7bccd4d349f08
Laurence Freeman <laurence@laurences-mbp.lan>
2020-11-16 06:41:37 +0000

    +

--- PredictLicking/is_licking.py
+++ PredictLicking/is_licking.py
@@ -1,37 +1,38 @@
-#Run this script with one argument in the command line. That argument should be the csv file output of DLC
 #This script is uses to decide whether the mouse is licking or not, and which spout
-# import credit_assignment_project.dlc
 import matplotlib.pyplot as plt
 import electrophysiology.ingest_timesync as ingest
 import PredictLicking.process_tongue_data as process
 import PredictLicking.is_licking as lick
 import numpy as np
 import pandas as pd
-import cv2
-import sys
-
-#test
+from decimal import *
 
 #Extend data print rows
 # pd.set_option("display.max_rows", None, "display.max_columns", None)
+# pd.set_option("display.precision", 10)
 
 def is_licking(csv_path):
+
     #Change file name to run function
     df = process.process_data_spout(csv_path)
-    df_len = process.process_data_spout(csv_path)
-    #Filter for any two features predicted at 99%--------------------------
+    df = df.set_index('Frames')
+    # df_len = process.process_data_spout(csv_path)
+    df = df.astype(float)
+
+    # Filter for any two features predicted at 99%--------------------------
     df = df.loc[(df['C_T_L'] >= 0.99) & (df['F_E_L'] >= 0.99)
                |(df['C_T_L'] >= 0.99) & (df['RE_T_L'] >= 0.99)
                |(df['C_T_L'] >= 0.99) & (df['LE_T_L'] >= 0.99)
                |(df['LE_T_L'] >= 0.99) & (df['RE_T_L'] >= 0.99)
                |(df['LE_T_L'] >= 0.99) & (df['F_E_L'] >= 0.99)
                |(df['RE_T_L'] >= 0.99) & (df['F_E_L'] >= 0.99)]
-    frame_is_licking = df.iloc[:,0].values
-    return(df, frame_is_licking, df_len)
+    frame_is_licking = df.index.values
+    frame_is_licking = np.array([int(x) for x in frame_is_licking])
+    return(df,frame_is_licking)
 
 def is_licking_spout(df, csv):
     #Create a dataframe with frames where mouse is licking
-    df, frame_is_licking, df_len = lick.is_licking(csv)
+    df, frame_is_licking = lick.is_licking(csv)
 
     #Find the middle X coord between cherry and grape spouts
     avg_LRight_GS_X = df["LR_GS_X"].mean()
@@ -40,27 +41,35 @@ def is_licking_spout(df, csv):
     Center_X = Center_X + avg_LRight_GS_X
 
     #Is licking left / grape spout
-    is_licking_grape = df.loc[(df['RE_T_X'] < df["LR_GS_X"])
-                             |(df['C_T_X'] < df["LR_GS_X"])
-                             &(df['LE_T_X'] < df["LR_GS_X"])
-                             |(df['LE_T_X'] < df["LC_GS_X"])
-                             |(df['RE_T_X'] < Center_X)]
+    is_licking_grape =  df.loc[(df['RE_T_X'] < df["LR_GS_X"])
+                              |(df['C_T_X']  < df["LR_GS_X"])
+                              &(df['LE_T_X'] < df["LR_GS_X"])
+                              |(df['LE_T_X'] < df["LC_GS_X"])
+                              |(df['LE_T_X'] < df["LR_GS_X"])
+                              |(df['RE_T_X'] < Center_X)]
 
+    #Is licking right / cherry spout
     is_licking_cherry = df.loc[(df['LE_T_X'] > df["RL_CS_X"])
-                             |(df['C_T_X'] > df["RL_CS_X"])
-                             &(df['RE_T_X'] > df["RL_CS_X"])
-                             |(df['RE_T_X'] > df["RC_CS_X"])
-                             |(df['LE_T_X'] > Center_X)]
-    frames_licking_cherry = is_licking_cherry.iloc[:,0].values
-    frames_licking_grape = is_licking_grape.iloc[:,0].values
+                              |(df['C_T_X']  > df["RL_CS_X"])
+                              &(df['RE_T_X'] > df["RL_CS_X"])
+                              |(df['RE_T_X'] > df["RC_CS_X"])
+                              |(df['RE_T_X'] > df["RL_CS_X"])
+                              |(df['LE_T_X'] > Center_X)]
+
+    #Convert data frame into values array
+    frames_licking_cherry = is_licking_cherry.index.values
+    frames_licking_grape = is_licking_grape.index.values
+    frames_licking_cherry = np.array([int(x) for x in frames_licking_cherry])
+    frames_licking_grape = np.array([int(x) for x in frames_licking_grape])
 
     #Is licking neither spout
     #Convert to lists to use remove function
     centre_lick = set(frame_is_licking.flat) - set(frames_licking_cherry.flat) - set(frames_licking_grape.flat)
+    centre_lick = np.array([int(x) for x in centre_lick])
     return(frames_licking_cherry,frames_licking_grape,centre_lick)
 
 def generate_licking_times(frametimes,dlc_csv):
-    df, frame_is_licking, df_len = lick.is_licking(dlc_csv)
+    df, frame_is_licking = lick.is_licking(dlc_csv)
     cherry_frames, grape_frames, center_frames = lick.is_licking_spout(df, dlc_csv)
 
     #Create three dfs for reward licking
@@ -76,12 +85,10 @@ def generate_licking_times(frametimes,dlc_csv):
                                                 how="left",left_on="frames licking",
                                                 right_index=True)
     cherry_licking_df.columns = ["Frames Licking", "Time Licking"]
-
     grape_licking_df = grape_licking_df.merge(frametimes_df,
                                                 how="left",left_on="frames licking",
                                                 right_index=True)
     grape_licking_df.columns = ["Frames Licking", "Time Licking"]
-
     center_licking_df = center_licking_df.merge(frametimes_df,
                                                 how="left",left_on="frames licking",
                                                 right_index=True)
--- electrophysiology/process_timesync.py
+++ electrophysiology/process_timesync.py
@@ -1,46 +1,39 @@
-from ingest_timesync import *
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import sys
-import math
-
-def process_timesync_data():
-    #Load_mat_file
-    df, spike_times, cluster_IDs = convert_mat('/Users/laurence/Desktop/Neuroscience/mproject/Data/aligned_physdata_KM011_2020-03-20_probe0.mat')
-    trial_df = df
-
-    #Create new spike_time df
-    spike_df =  pd.DataFrame(spike_times, columns = ["Spike_Times"])
-    spike_df["Trial"] = np.nan
-    spike_df["cluster_ids"] = cluster_IDs
-    spike_and_cluster_df = spike_df
-
-    #Remove limitations to printing df
-    # pd.set_option("display.max_rows", None, "display.max_columns", None)
-
-    #Assign bins to each spike time using trial start time and the cut function
-    trial_start_times = np.asarray(df.iloc[:,-1])
-    spike_df["Trial"] = pd.cut(spike_df["Spike_Times"], trial_start_times)
-
-    #Create new column and substract trial start time from each spike time
-    # print(type(spike_df["Trial"][386828].left))
-    spike_df["Lower Bound"] = (spike_df["Trial"].apply(lambda x: x.left))
-    spike_df["Lower Bound"] = spike_df["Lower Bound"].astype(float)
-
-    #Test for spike length
-    # print("The lenght of the spike df",spike_df)
-    # print(spike_df[["reward_times","Spike_Times"]])
-
-    #Remove all rows with NaN that occur before the trials start or after trials have finsished
-    # print(spike_df)
-    spike_df = spike_df.dropna()
-
-    #Merge two dataframes
-    #First create an ID between each df that can be used for the merge - Use trial start time
-    #trunc removes numbers after decimal, used to create ID
-    trunc = lambda x: math.trunc(x)
-    spike_df["Trial ID"] = spike_df["Lower Bound"].apply(trunc)
-    df["Trial ID"] = df["trial_start_times"].apply(trunc)
-    df = spike_df.merge(df, on="Trial ID")
-    return(df, trial_df, spike_and_cluster_df)
+# from ingest_timesync import *
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import sys
+# import math
+#
+# def process_timesync_data():
+#     #Load_mat_file
+#     df, spike_times, cluster_IDs = convert_mat('/Users/laurence/Desktop/Neuroscience/mproject/Data/aligned_physdata_KM011_2020-03-20_probe0.mat')
+#     trial_df = df
+#
+#     #Create new spike_time df
+#     spike_df =  pd.DataFrame(spike_times, columns = ["Spike_Times"])
+#     spike_df["Trial"] = np.nan
+#     spike_df["cluster_ids"] = cluster_IDs
+#     spike_and_cluster_df = spike_df
+#
+#     #Assign bins to each spike time using trial start time and the cut function
+#     trial_start_times = np.asarray(df.iloc[:,-1])
+#     spike_df["Trial"] = pd.cut(spike_df["Spike_Times"], trial_start_times)
+# 
+#     #Create new column and substract trial start time from each spike time
+#     # print(type(spike_df["Trial"][386828].left))
+#     spike_df["Lower Bound"] = (spike_df["Trial"].apply(lambda x: x.left))
+#     spike_df["Lower Bound"] = spike_df["Lower Bound"].astype(float)
+#
+#     #Remove all rows with NaN that occur before the trials start or after trials have finsished
+#     # print(spike_df)
+#     spike_df = spike_df.dropna()
+#
+#     #Merge two dataframes
+#     #First create an ID between each df that can be used for the merge - Use trial start time
+#     #trunc removes numbers after decimal, used to create ID
+#     trunc = lambda x: math.trunc(x)
+#     spike_df["Trial ID"] = spike_df["Lower Bound"].apply(trunc)
+#     df["Trial ID"] = df["trial_start_times"].apply(trunc)
+#     df = spike_df.merge(df, on="Trial ID")
+#     return(df, trial_df, spike_and_cluster_df)
