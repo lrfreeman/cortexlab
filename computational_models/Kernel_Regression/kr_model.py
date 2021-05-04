73de1e28aa5d48b453adadf394d1a3192ba1038f
Laurence Freeman <laurence.freeman@live.co.uk>
2021-05-04 20:51:21 +0100

    Refactor kernel regression

--- computational_models/Kernel_Regression/kr_model.py
+++ computational_models/Kernel_Regression/kr_model.py
@@ -25,13 +25,15 @@ spike_clusters = np.load("/Users/laurence/Desktop/spike_clusters.npy")
 # first_lick_times = np.asarray(first_lick_df["First Lick Times"])
 
 """Free Parameters"""
-time_bin = 0.1 # time bin size for data in seconds
+time_bin = 0.2 # time bin size for data in seconds
 sampling_rate = 30000 # sampling rate of probe in Hz
 cell_ID = 0 #Define the unit to filter spike times on
-synthetic_trial_number = 10000
+synthetic_trial_number = 500
 
-#Defines how big kernels should be
+#Defines how big kernels should be - must be intergers
 shift_back = 15 #1.5seconds as bin count is set to 0.1 above
+# shift_back = 5 #0.5seconds as bin count is set to 0.1 above
+# shift_forward = 15 #1.5seconds as bin count is set to 0.1 above
 shift_forward = 30 #3seconds as bin count is set to 0.1 above
 shift_total = shift_back + shift_forward
 kernal_window_range = np.arange(-shift_back,shift_forward,1).tolist()
@@ -44,19 +46,23 @@ end_time = np.max(spike_times) + 10  # End-point in seconds of the time period y
 """Create synethic reward times"""
 reward_times = np.random.default_rng().uniform(0, end_time, synthetic_trial_number)
 
+# print("Synthetic reward times", reward_times)
+
 """Create synethic lick times"""
 #Create lick times distributed across reward times
-# first_lick_times = [np.random.default_rng().normal(loc = time, scale = 0.2) for time in reward_times]
+#Add one second so as to always be after reward
+# first_lick_times = [np.random.default_rng().normal(loc = time, scale = 0.2) + 1 for time in reward_times]
 
 #Create lick times randomly
 first_lick_times = np.random.default_rng().uniform(0, end_time, synthetic_trial_number)
 
+# print("Synthetic lick times", first_lick_times)
 
 """Outline the kernels"""
 kernels = [reward_times, first_lick_times] # Each item should be an event kernel
 
 """Create synethic spike times for X"""
-length = 1000000 #Create an artificial number of spikes
+length = 100000 #Create an artificial number of spikes
 spike_clusters = np.asarray(np.random.randint(2, size=(length,1))) #Create a vector of 0's and 1's / 2x units
 
 #The below code makes a list of lists for spike times such as: [[23s], [34s], [35s]] over a mean of reward time
@@ -71,21 +77,6 @@ while x < length: #Loop until spikes = lenght
 
 spike_times = np.asarray(spike_times)
 spike_times = spike_times[0:length]
-
-"""Other code to make syth x to probe negative coeff bug"""
-#Won't work if uncommented as generates a batch of spike times in one loop with the overall vector needing to match the length variable
-# spike_times = []
-# for time in reward_times:
-#     spike_times.append(np.random.default_rng().normal(loc = time, scale = 0.1, size = 500))
-#
-# flat_list = [item for sublist in spike_times for item in sublist]
-#
-# def extractDigits(lst):
-#     return [[el] for el in lst]
-#
-# spike_times = extractDigits(flat_list)
-# spike_times = np.asarray(spike_times)
-# spike_times = spike_times[0:length]
 
 """Create the Y output"""
 spike_data = pd.DataFrame(spike_times, columns = ["spike_times"])
@@ -102,52 +93,19 @@ def histogram(spike_df, cell_ID):
 
     return(session_counts)
 
+"""Define  Y""" # Spike counts assigned to each bin
+
 Y = np.asarray(histogram(spike_data, cell_ID))
 
-"""Plot spike distribution"""
+"""Plot spike distribution for checking synethic data"""
 # plt.figure()
 # plt.plot(bins, Y)
 # plt.title("Histogram of synethic spikes")
 # plt.show()
 
 """Create design matrix"""
-#
-# design_matrix_dic = {}
-#
-# def create_design_matrix(kernels):
-#
-#     for z in range(len(kernels)):
-#
-#         map_event_to_bin = np.digitize(kernels[z], bins) #Return the indices of the bins to which each value in input array belongs.
-#         bin_vector = np.zeros((len(bins), 1)) #Creates a vector of zeros of lenghh bin
-#
-#         #Some length off by one bug where the last bin sends an index error so Ive excluded it with -2 index
-#         for x in map_event_to_bin[:-2]:
-#             bin_vector[x] = 1  #Creates a vector of 1's and 0's where 1's refers to the event time index of a bin
-#
-#         #Create the design matrix
-#         i = 1 #start the index at 1 as to not break the looping logic
-#         shifted_vector = bin_vector[shift_back + shift_forward + 1 - i:-i]
-#         list = np.arange(2,46,1)
-#         design_matrix = pd.DataFrame(shifted_vector) #Creates a dataframe of a shifted event vector with column header 0
-#
-#         for i in list: #Starting at 2
-#             shifted_vector = bin_vector[shift_back + shift_forward + 1 - i:-i]
-#             design_matrix[str(i - 1)] = shifted_vector #Adds to the dataframe for the size of the kernel window, each column is a param
-#
-#         design_matrix_dic[str(z)] = design_matrix
-#
-#     return (design_matrix_dic)
-#
-# design_matrix_dic = create_design_matrix(kernels)
-# design_matrix = design_matrix_dic["0"]
-# print(design_matrix)
-# # design_matrix = pd.concat([design_matrix_dic["0"], design_matrix_dic["1"]], axis=1)
-# print("New Design Matrix Shape:", design_matrix.shape)
 
-"""Create new design matrix"""
-
-def create_design_matrix(kernel, naming_int):
+def create_design_matrix(kernel):
 
         map_event_to_bin = np.digitize(kernel, bins) #Return the indices of the bins to which each value in input array belongs.
         bin_vector = np.zeros((len(bins), 1)) #Creates a vector of zeros of lenghh bin
@@ -156,33 +114,35 @@ def create_design_matrix(kernel, naming_int):
         for x in map_event_to_bin[:-2]:
             bin_vector[x] = 1  #Creates a vector of 1's and 0's where 1's refers to the event time index of a bin
 
-        #Create the design matrix
         i = 1 #start the index at 1 as to not break the looping logic
         shifted_vector = bin_vector[shift_back + shift_forward + 1 - i:-i]
-        list = np.arange(2,46,1)
+        kernel_window_len_interator = np.arange(2,shift_total,1)
         design_matrix = pd.DataFrame(shifted_vector) #Creates a dataframe of a shifted event vector with column header 0
 
-        for i in list: #Starting at 2
+        for i in kernel_window_len_interator: #Starting at 2 because the first bin is used in the skeleton dataframe
             shifted_vector = bin_vector[shift_back + shift_forward + 1 - i:-i]
-            design_matrix[str(i - 1 + naming_int)] = shifted_vector #Adds to the dataframe for the size of the kernel window, each column is a param
+            design_matrix[str(i - 1)] = shifted_vector #Adds to the dataframe for the size of the kernel window, each column is a param
 
         return (design_matrix)
 
-# design_matrix = create_design_matrix(reward_times, 0)
-design_matrix_reward = create_design_matrix(reward_times, 0)
-design_matrix_lick = create_design_matrix(first_lick_times, 0)
-design_matrix = pd.concat([design_matrix_reward, design_matrix_lick], axis=1)
+design_matrix = create_design_matrix(reward_times)
+# design_matrix_reward = create_design_matrix(reward_times)
+# design_matrix_lick = create_design_matrix(first_lick_times)
+# design_matrix = pd.concat([design_matrix_reward, design_matrix_lick], axis=1)
 
 """Calculate coefficient matrix"""
-R1 = np.corrcoef(design_matrix_reward, design_matrix_lick, rowvar=False)
-sns.heatmap(R1, cmap = "GnBu", yticklabels=False, xticklabels=False)
-plt.show()
+# R1 = np.corrcoef(design_matrix_reward, design_matrix_lick, rowvar=False)
+# sns.heatmap(R1, cmap = "GnBu", yticklabels=False, xticklabels=False)
+# plt.show()
 
 """Set up design matrix to run within sm.GLM"""
 X = sm.add_constant(design_matrix, prepend=False)
 
+"""Calculate the delta between x and Y caused by differing kernel size"""
+delta = Y.shape[0] - X.shape[0]
+
 """Code to run sm.GLM"""
-model = sm.GLM(Y[46:], X, family = sm.families.Gaussian()).fit() #Remove the first 46 elements of Y to match the shape of the design matrix
+model = sm.GLM(Y[delta:], X, family = sm.families.Gaussian()).fit() #Remove the first 46 elements of Y to match the shape of the design matrix
 summary_of_model = model.summary()
 weights = model.params
 weights = np.asarray(weights) #Constant is at the endd of the vector
@@ -193,8 +153,8 @@ bin_centres = 0.5*(kernal_window_range[1:] + kernal_window_range[:-1]) / 10 # to
 
 """""Plotting logic"""
 plt.figure()
-plt.plot(bin_centres, weights[:44], label = 'Reward Kernel') # cut last weight off ddue to use of bin centres
-plt.plot(bin_centres, weights[44:-3], label = 'First Lick Kernel') # cut constant and last weighht off
+plt.plot(bin_centres, weights[:(shift_total - 1)], label = 'Reward Kernel') # cut last weight off ddue to use of bin centres
+# plt.plot(bin_centres, weights[shift_total - 1: -1], label = 'First Lick Kernel') # cut constant and last weighht off
 plt.xlabel("Kernel Window (seconds)", fontsize = 12)
 plt.ylabel("Coefficients", fontsize = 12)
 # plt.title("Kernel Regression: cluster ID {} ".format(cell_ID), fontsize = 12)
